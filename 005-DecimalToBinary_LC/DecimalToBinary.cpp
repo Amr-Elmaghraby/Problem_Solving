5ef018169dd90f3b0fb5cc3c5ec0ad915b83038f
Amr Elmaghraby <110898846+Amr-Elmaghraby@users.noreply.github.com>
2024-09-30 04:26:54 +0300

    Update DecimalToBinary.cpp
    edit directly

--- 005-DecimalToBinary_LC/DecimalToBinary.cpp
+++ 005-DecimalToBinary_LC/DecimalToBinary.cpp
@@ -5,15 +5,13 @@ using namespace std;
 
 class Solution{
     public:
-    bool isPalindrome(int x){
-        bool out = true;
+    void isPalindrome(int x){
         int x_b=0;int factor=1;
         for(int i =0 ; i<=31;++i){
             x_b += factor * (x>>i & 1);
             factor *=10;
         }
         cout << x_b;
-        return out;
     }
 };
 
@@ -21,8 +19,6 @@ class Solution{
 int main(){
     Solution sol;
     int input = 121;
-    bool out = sol.isPalindrome(input);
+    sol.isPalindrome(input);
 
-
-    // cout << out << endl;
 }
